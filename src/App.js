efa9fb787b8109b31a7a7e66c21e378d2492c000
Sergey <GreyTob@inbox.ru>
2021-07-05 22:17:41 +0500

    Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import classes from './App.module.scss'
+import { Switch, Route, Redirect, NavLink } from 'react-router-dom'
+import Profile from './components/Profile/Profile'
+import Users from './components/Users/Users'
 
 function App() {
   return (
     <div className={classes.App}>
-      <h1>Hello W!</h1>
+      <NavLink to="/">Users</NavLink>
+      <NavLink to="/profile">Profile</NavLink>
+
+      <Switch>
+        <Route path="/" exact component={Users} />
+        <Route path="/profile" component={Profile} />
+        {/* <Route path="/profile:name" component={Profile} /> */}
+        <Redirect path="/" />
+      </Switch>
     </div>
   )
 }
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,7 @@
+export default function Profile() {
+  return (
+    <div>
+      <h2>One User</h2>
+    </div>
+  )
+}
